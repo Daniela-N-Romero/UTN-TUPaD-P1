abfeb48bfbe38ff64d2fe295dbe5cbc28d1852d2
Daniela Romero <danielanahirromero@gmail.com>
2025-04-25 19:30:42 -0300

    Termine el TP de Listas

--- 06 Datos complejos/ROMERO-DANIELA-TP6-LISTAS.py
+++ 06 Datos complejos/ROMERO-DANIELA-TP6-LISTAS.py
@@ -3,48 +3,63 @@
 #EJERCICIO 1
 multiplos_de_4 = list(range(0,101,4))
 multiplos_de_4.remove(0)
-print(multiplos_de_4)
+print(f"Los multiplos de 4 son: {multiplos_de_4}")
 
 #EJERCICIO 2
 nombres = ["Laura", "Daniela", "Joaquin", "Lucas", "Sofia"]
-print(nombres[-1])
-print(nombres[4])
+print(f"El nombre en la última posición es: {nombres[-1]}")
+print(f"El nombre en la última posición es: {nombres[4]}")
 
 #EJERCICIO 3
 lista_vacia = []
 lista_vacia.append("Hola")
 lista_vacia.append("Mundo")
 lista_vacia.append("Python")
-print(lista_vacia)
+print(f"Los elementos de la lista ahora son: {lista_vacia}")
 
 #EJERCICIO 4
 animales = ["perro", "gato", "conejo", "pez"]
 animales[2] = "loro"
 animales[-1] = "oso"
-print(animales)
+print(f"Los animales ahora son: {animales}")
 
 #EJERCICIO 5
 numeros = [8,15,3,22,7]
 numeros.remove(max(numeros))
-print(numeros)
+print(f"Los numeros ahora son: {numeros}")
 #El programa elimina el numero mayor de la lista y luego imprime la lista modificada.
 
 #EJERCICIO 6
 numeros = list(range(10,31,5))
-print(numeros)
-print(numeros[0:2])
+print(f"Los numeros son: {numeros}")
+print(f"Los primeros dos numeros son: {numeros[0:2]}")
 
 #EJERCICIO 7
 autos = ["sedan", "polo", "suran", "gol"]
 autos[1] = "chronos"
 autos[2] = "focus"
-print(autos)
+print(f"Los autos ahora son: {autos}")
 
 #EJERCICIO 8
+dobles = []
+for i in range(5,16,5):
+    dobles.append(i*2)
+print(f"Los dobles son: {dobles}")
 
 #EJERCICIO 9
 compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
 compras[2].append("jugo")
 compras[1][1] = "tallarines"
 compras[0].remove("pan") 
-print(compras) 
+print(f"Las compras ahora son: {compras}") 
+
+#EJERCICIO 10
+
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+print(f"Los elementos de la lista anidada son:")
+print(f"lista_anidada[0]: {lista_anidada[0]}") 
+print(f"lista_anidada[1]: {lista_anidada[1]}")
+print(f"lista_anidada[2][0]: {lista_anidada[2][0]}")
+print(f"lista_anidada[2][1]: {lista_anidada[2][1]}")
+print(f"lista_anidada[2][2]: {lista_anidada[2][2]}")
+print(f"lista_anidada[3]: {lista_anidada[3]}")
